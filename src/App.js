985e2cf8bfb5d67686c0c51b8ece64e3ff7758ea
You Song Hou <yousonghou@yahoo.com>
2021-05-17 23:54:50 -0400

    set up create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Secret Funder</h1>
+    </div>
+  );
+}
+
+export default App;
