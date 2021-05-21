cc3e0549bb7dfb44e4d186734538784ccd41c25b
You Song Hou <yousonghou@yahoo.com>
2021-05-21 00:35:57 -0400

    add a route to mint nft

--- server/routes/network.js
+++ server/routes/network.js
@@ -8,24 +8,24 @@ require('dotenv').config();
 
 const customFees = {
   upload: {
-    amount: [{ amount: '2000000', denom: 'uscrt' }],
-    gas: '2000000',
+    amount: [{ amount: "2000000", denom: "uscrt" }],
+    gas: "2000000",
   },
   init: {
-    amount: [{ amount: '500000', denom: 'uscrt' }],
-    gas: '500000',
+    amount: [{ amount: "500000", denom: "uscrt" }],
+    gas: "500000",
   },
   exec: {
-    amount: [{ amount: '500000', denom: 'uscrt' }],
-    gas: '500000',
+    amount: [{ amount: "500000", denom: "uscrt" }],
+    gas: "500000",
   },
   send: {
-    amount: [{ amount: '80000', denom: 'uscrt' }],
-    gas: '80000',
+    amount: [{ amount: "80000", denom: "uscrt" }],
+    gas: "80000",
   },
 };
 
-router.get('/getcounter', async (req, res, next) => {
+router.post('/createnft', async (req, res, next) => {
   const httpUrl = process.env.SECRET_REST_URL;
   const mnemonic = process.env.MNEMONIC;
 
@@ -47,57 +47,34 @@ router.get('/getcounter', async (req, res, next) => {
     txEncryptionSeed, customFees,
   );
   console.log(`Wallet address=${accAddress}`);
-
-  const contractAddress = "secret1c8cmrxuaumhefrnujc7rtrmy5dh4nn5tlafth7";
-  // Query the counter
-  console.log('Querying contract for current count');
-  let response = await client.queryContractSmart(contractAddress, { get_count: {} })
-    .catch((err) => { throw new Error(`Could not query contract: ${err}`); });
-
-  return res.status(200).json({
-    'count': response.count
-  });
-});
-
-router.put('/addcounter', async (req, res, next) => {
-  const httpUrl = process.env.SECRET_REST_URL;
-  const mnemonic = process.env.MNEMONIC;
-
-  const signingPen = await Secp256k1Pen.fromMnemonic(mnemonic)
-    .catch((err) => { throw new Error(`Could not get signing pen: ${err}`); });
-
-  // Get the public key
-  const pubkey = encodeSecp256k1Pubkey(signingPen.pubkey);
-
-  // Get the wallet address
-  const accAddress = pubkeyToAddress(pubkey, 'secret');
-
-  // Initialize client
-  const txEncryptionSeed = EnigmaUtils.GenerateNewSeed();
-  const client = new SigningCosmWasmClient(
-    httpUrl,
-    accAddress,
-    (signBytes) => signingPen.sign(signBytes),
-    txEncryptionSeed, customFees,
-  );
-  console.log(`Wallet address=${accAddress}`);
-
-  const contractAddress = "secret1c8cmrxuaumhefrnujc7rtrmy5dh4nn5tlafth7";
-
-  // Increment the counter
-  const handleMsg = { increment: {} };
-  console.log('Updating count');
-  response = await client.execute(contractAddress, handleMsg)
-    .catch((err) => { throw new Error(`Could not execute contract: ${err}`); });
-  console.log('response: ', response);
   
-  // Query again to confirm it worked
-  console.log('Querying contract for updated count');
-  response = await client.queryContractSmart(contractAddress, { get_count: {} })
-    .catch((err) => { throw new Error(`Could not query contract: ${err}`); });
+  // Define your metadata
+  const publicMetadata = "<public metadata>";
+  const privateMetadata = "<private metadata>";
+
+  // Mint a new token
+  const handleMsg = {
+    mint_nft: {
+      owner: accAddress,
+      public_metadata: {
+        name: publicMetadata,
+      },
+      private_metadata: {
+        name: privateMetadata,
+      },
+    },
+  };
+
+  console.log("Minting a nft");
+  const response = await client
+    .execute(process.env.SECRET_NFT_CONTRACT, handleMsg)
+    .catch((err) => {
+      throw new Error(`Could not execute contract: ${err}`);
+    });
+  console.log("response: ", response);
 
   return res.status(200).json({
-    'count': response.count
+    'data': response
   });
 });
 
