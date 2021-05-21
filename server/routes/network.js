76ed01b3f18ef87e7efcea57eff4b9010c301ebf
You Song Hou <yousonghou@yahoo.com>
2021-05-21 01:57:39 -0400

    add a route to get all nfts

--- server/routes/network.js
+++ server/routes/network.js
@@ -25,8 +25,9 @@ const customFees = {
   },
 };
 
+const httpUrl = process.env.SECRET_REST_URL;
+
 router.post('/createnft', async (req, res, next) => {
-  const httpUrl = process.env.SECRET_REST_URL;
   const mnemonic = process.env.MNEMONIC;
 
   const signingPen = await Secp256k1Pen.fromMnemonic(mnemonic)
@@ -73,8 +74,86 @@ router.post('/createnft', async (req, res, next) => {
     });
   console.log("response: ", response);
 
-  return res.status(200).json({
+  return res.status(201).json({
     'data': response
+  });
+});
+
+router.get('/nfts', async (req, res, next) => {
+  const mnemonic = process.env.MNEMONIC;
+
+  // A pen is the most basic tool you can think of for signing.
+  // This wraps a single keypair and allows for signing.
+  const signingPen = await Secp256k1Pen.fromMnemonic(mnemonic).catch((err) => {
+    throw new Error(`Could not get signing pen: ${err}`);
+  });
+
+  // Get the public key
+  const pubkey = encodeSecp256k1Pubkey(signingPen.pubkey);
+
+  // get the wallet address
+  const accAddress = pubkeyToAddress(pubkey, 'secret');
+
+  // initialize client
+  const txEncryptionSeed = EnigmaUtils.GenerateNewSeed();
+
+  const client = new SigningCosmWasmClient(
+    httpUrl,
+    accAddress,
+    (signBytes) => signingPen.sign(signBytes),
+    txEncryptionSeed,
+    customFees
+  );
+  console.log(`Wallet address=${accAddress}`);
+
+  // 1. Get a list of all tokens
+  let queryMsg = {
+    tokens: {
+      owner: accAddress,
+    },
+  };
+
+  console.log("Reading all tokens");
+
+  let response = await client
+    .queryContractSmart(process.env.SECRET_NFT_CONTRACT, queryMsg)
+    .catch((err) => {
+      throw new Error(`Could not execute contract: ${err}`);
+    });
+  console.log("response: ", response.token_list.tokens);
+
+  if(response.token_list.tokens.length == 0){
+    return res.status(404).json({
+      'error': 'No token was found for you account, make sure that the minting step completed successfully'
+    });
+  }
+
+  const nftList = [];
+  const list = response.token_list.tokens;
+
+  for(let i = 0; i < list.length; i++){
+    const token_id = list[i];
+
+    // Query the public metadata
+    queryMsg = {
+      nft_info: {
+        token_id: token_id,
+      },
+    };
+
+    console.log(`Query public data of token #${token_id}`);
+    response = await client
+      .queryContractSmart(process.env.SECRET_NFT_CONTRACT, queryMsg)
+      .catch((err) => {
+        throw new Error(`Could not execute contract: ${err}`);
+      });
+    console.log("response: ", response);
+
+    nftList.push(response.nft_info);
+  }
+
+  return res.status(200).json({
+    'data': nftList
   });
 });
 
