933a554de300184c0afd3631d3c8809dd84d1487
You Song Hou <yousonghou@yahoo.com>
2021-05-22 01:40:34 -0400

    add burner-wallet feature using secretjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container } from 'semantic-ui-react';
 
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState('');
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar walletAddress={walletAddress} setWalletAddress={setWalletAddress} />
+      <Container>
+        <p>{walletAddress}</p>
+      </Container>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,36 @@
-import React, { useState } from 'react';
-import { Menu, Button } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react';
+import {
+  Secp256k1Pen, pubkeyToAddress, encodeSecp256k1Pubkey
+} from "secretjs";
+import { Menu, Button } from 'semantic-ui-react';
+import { Bip39, Random } from "@iov/crypto";
 
-function Navbar() {
+function Navbar({ walletAddress, setWalletAddress }) {
   const [loggedIn, setLoggedIn] = useState(null);
+
+  useEffect(() => {
+    async function burnerWallet(mnemonic){
+      const pen = await Secp256k1Pen.fromMnemonic(mnemonic);
+      const pubkey = encodeSecp256k1Pubkey(pen.pubkey);
+      const address = pubkeyToAddress(pubkey, "secret");
+      const signer = (signBytes) => pen.sign(signBytes);
+      console.log({ address, signer });
+      setWalletAddress(address);
+    }
+
+    const key = "burner-wallet";
+    const loaded = localStorage.getItem(key);
+    if (loaded) {
+      console.log(loaded);
+      burnerWallet(loaded);
+    }
+    else {
+      const generated = Bip39.encode(Random.getBytes(16)).toString();
+      localStorage.setItem(key, generated);
+      console.log(generated);
+      burnerWallet(generated);
+    }
+  }, [])
 
   return (
     <Menu color="blue" inverted pointing>
@@ -10,13 +38,12 @@ function Navbar() {
         to="/"
         name='Secret Funder'
       />
-      {loggedIn ? (
+      {walletAddress ? (
         <Menu.Menu position='right'>
           <Menu.Item>
-            <Button secondary >Logout</Button>
+            <Button secondary>Disconnect</Button>
           </Menu.Item>
         </Menu.Menu>
-
       ) : (
         <Menu.Menu position='right'>
           <Menu.Item>
