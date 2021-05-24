172d5dee59ddf2fefa8e21d6b31c5b91ad86e00b
You Song Hou <yousonghou@yahoo.com>
2021-05-23 20:43:45 -0400

    add an image uploader that sends the image to Pinata

--- src/pages/AddForm.js
+++ src/pages/AddForm.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
 import { Container, Card, Form, Button } from 'semantic-ui-react';
 
+import { pinataApiKey, pinataSecretApiKey } from '../config';
+import axios from '../axios';
+
 function AddForm() {
   const [title, setTitle] = useState('');
   const [image, setImageURL] = useState('');
   const [body, setBody] = useState('');
+
+  const getFileAndUploadONPinata = async event => {
+    try{
+      const file = event.target.files[0];
+
+      let formData = new FormData();
+      formData.append('file', file);
+  
+      const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+        maxContentLength: "Infinity",
+        headers: {
+          "Content-Type": 'multipart/form-data',
+          pinata_api_key: pinataApiKey, 
+          pinata_secret_api_key: pinataSecretApiKey,
+        }
+      })
+      console.log(res);
+      setImageURL(res.data.IpfsHash);
+    } catch(err) {
+      console.error(err);
+    }
+  }
 
   return (
     <Container>
@@ -17,7 +42,7 @@ function AddForm() {
             </Form.Field>
             <Form.Field>
               <label>Image URL</label>
-              <input value={image} onChange={(e) => setImageURL(e.target.value)} />
+              <input type="file" onChange={getFileAndUploadONPinata} />
             </Form.Field>
             <Form.TextArea label='Detail' value={body} onChange={(e) => setBody(e.target.value)} />
             <Button
